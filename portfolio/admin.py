8494c13fe6b4beed4814886881374f0400f2bc49
Vedran Olujic <vedranolujic23@gmail.com>
2021-06-06 17:21:47 +0200

    Initial setup of project, enabled generation of portfolio items in Django /admin URL.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project
+from django.db import models
 
-# Register your models here.
+#admin.site.register(Project)
+
+
+@admin.register(Project)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
