4b65e971236a714828f2533e339c1a6e38bd344b
Vedran Olujic <vedranolujic23@gmail.com>
2021-11-27 19:26:45 +0100

    fixed up home page, working version

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 #from this directory
 from blog import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.all_blogs, name = 'all'),
+    path('', views.all_blogs, name='all'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+
 
 ]
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,10 +20,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     #anything blog related - forwarded to urls.py in blog folder
-    path('blog/', include('blog.urls'))]
+    path('blog/', include('blog.urls')),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
